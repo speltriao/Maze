bdd623e299eeb991e642c32b3b3d8ad1f0fff1b7
Alexandre Speltri <alexandreporks@gmail.com>
2022-08-22 14:09:46 -0300

    Add files via upload

--- src/run.hpp
+++ src/run.hpp
@@ -1,9 +1,9 @@
 void welMsg(){                     
-   std::cout<<"  /\\/\\    __ _  ____  ___"<<"\n";
-    std::cout<<" /    \\  / _` ||_  / / _ \\"<<"\n";
-    std::cout<<"/ /\\/\\ \\| (_| | / / |  __/"<<"\n";
-    std::cout<<"\\/    \\/ \\__,_|/___| \\___|"<<"\n";
-    std::cout<<UNDER<<"\n\nplay with WASD keys\n\n"<<RESET;                              
+   std::cout << "  /\\/\\    __ _  ____  ___"<<"\n";
+    std::cout << " /    \\  / _` ||_  / / _ \\"<<"\n";
+    std::cout << "/ /\\/\\ \\| (_| | / / |  __/"<<"\n";
+    std::cout << "\\/    \\/ \\__,_|/___| \\___|"<<"\n";
+    std::cout << UNDER << "\n\nplay with WASD keys\n\n" << RESET;                              
 }
 
 int runGame(){
